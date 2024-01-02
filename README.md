30c90b7420f06b9ba0fc66e1c1cc7ea3204a060c
CaffeeLake <PascalCoffeeLake@gmail.com>
2024-01-03 07:04:00 +0900

    Add support keep windows size.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Bluesky Electron Client
 
-[Blueskyの公式Webクライアント](https://staging.bsky.app/)を単体アプリとして利用できるElectronを使ったクライアントです。<br />
+[Blueskyの公式Webクライアント](https://bsky.app/)を単体アプリとして利用できるElectronを使ったクライアントです。<br />
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
 // Packages
-import { BrowserWindow, app } from 'electron';
+import { app, BrowserWindow } from 'electron';
+import Store from 'electron-store';
+import path from 'path';
+
+path.join(app.getPath('userData'), 'config.json');
+
+const store = new Store({
+  configFileMode: 0o755,
+  defaults: {
+    x: undefined,
+    y: undefined,
+    width: 1280,
+    height: 720,
+  },
+});
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
   const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    autoHideMenuBar: true,
+    x: store.get('x'),
+    y: store.get('y'),
+    width: store.get('width'),
+    height: store.get('height'),
   });
 
-  const url = 'https://staging.bsky.app/';
+  const url = 'https://bsky.app/';
 
   mainWindow.loadURL(url);
+
+  mainWindow.once('close', () => {
+    const { x, y, width, height } = mainWindow.getBounds();
+    store.set('x', x);
+    store.set('y', y);
+    store.set('width', width);
+    store.set('height', height);
+  });
 });
 
 // Quit the app once all windows are closed
